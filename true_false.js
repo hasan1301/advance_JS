6e17a4396d7d6daef8cd4770d3f028ae03991f33
Hasan Khan <76878543+hasan1301@users.noreply.github.com>
2021-06-21 03:27:02 +0600

    add file

---
+++ true_false.js
@@ -0,0 +1,21 @@
+// 0 is false value other true value
+
+let age = 0
+
+if (age) {
+    console.log('condition is true');
+}
+
+else {
+    console.log('condition is false');
+}
+
+let name = 'amir'
+
+if (name) {
+    console.log('condition is true');
+}
+
+else {
+    console.log('condition is false');
+}
