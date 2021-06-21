6080a90b33372d15c8d12cd42297a3d8731e2dd8
Hasan Khan <76878543+hasan1301@users.noreply.github.com>
2021-06-21 14:32:50 +0600

    file add

---
+++ array-slice.js
@@ -0,0 +1,9 @@
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let part = nums.slice(1, 3);
+let part2 = nums.splice(1, 3)
+
+let together = nums.join(' ');
+
+console.log(together);
+// console.log(part);
+// console.log(part2);
