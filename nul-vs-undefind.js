df53299ba52297ef835d31251adc21f2c076232f
Hasan Khan <76878543+hasan1301@users.noreply.github.com>
2021-06-21 11:59:06 +0600

    file add

---
+++ nul-vs-undefind.js
@@ -0,0 +1,16 @@
+let pakhi;
+
+console.log(pakhi);
+
+function add(n1, n2) {
+    console.log(n1 + n2);
+}
+
+let result = add(12, 16)
+
+console.log(result);
+
+let fun = undefined
+
+let ages = [2, 6, 7, 6]
+console.log(ages[6]);
