dec10012431df7602fac3092ec1f557bfbb71208
Hasan Khan <76878543+hasan1301@users.noreply.github.com>
2021-06-21 12:44:03 +0600

    file add

---
+++ double-vs-triple_equal.js
@@ -0,0 +1,20 @@
+let first = 2
+let second = '2'
+
+if (first == second) {
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
+
+
+let a = 2
+let b = '2'
+
+if (a === b) {
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
---
+++ map-filter.js
@@ -0,0 +1,29 @@
+let numbers = [2, 3, 4, 5, 6]
+let output = []
+
+
+for (let i = 0; i < numbers.length; i++) {
+    let element = numbers[i];
+    let result = element * element
+    output.push(result)
+}
+
+console.log(output);
+
+function square(element) {
+    return element * element;
+}
+
+let result = numbers.map(function (element) {
+    return element * element
+})
+
+console.log(result);
+
+let result2 = numbers.map(x => x * x)
+
+console.log(result2);
+
+
+let bigger = numbers.filter(x => x > 4)
+console.log(bigger);
